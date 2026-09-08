23a52f26b83655e18435ce1a41785c79fcc2b408
d6mjdz26dw-blip <d6mjdz26dw@privaterelay.appleid.com>
2026-09-08 12:55:17 +0900

    0908uploaqd
    기초

---
+++ week_2HTML 5 basic/Outerjavascript.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>HTML 5Basic</title>
+    <script src="outerjavascript.js"></script>
+</head>
+<body>
+    <h1>Hello world...!</h1>
+</body>
+</html>
